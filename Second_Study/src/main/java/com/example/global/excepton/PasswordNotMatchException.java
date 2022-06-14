27540afcae8c4fde798078bbf622dba02f552e4d
Juyeong <103026521+jyk1029@users.noreply.github.com>
2022-06-14 22:13:35 +0900

    🖤 :: SignUp, SignIn, Update, Delete, List, Error, Exception

---
+++ Second_Study/src/main/java/com/example/global/excepton/PasswordNotMatchException.java
@@ -0,0 +1,11 @@
+package com.example.global.excepton;
+
+import com.example.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class PasswordNotMatchException extends RuntimeException {
+    private PasswordNotMatchException(){
+        super(ErrorCode.PASSWORD_NOT_MATCHED.getMessage());
+    }
+}
