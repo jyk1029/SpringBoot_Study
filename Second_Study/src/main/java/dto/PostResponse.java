9eb889127d4fe00743340b9d4e7c95394c50cd87
Juyeong <103026521+jyk1029@users.noreply.github.com>
2022-06-29 23:31:56 +0900

    🖤 :: SignUp, SignIn, Update, Delete, Exception

---
+++ Second_Study/src/main/java/dto/PostResponse.java
@@ -0,0 +1,16 @@
+package Dto;
+
+import com.example.SpringBoot.Sex;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PostResponse {
+    private final String accountId;
+    private final String password;
+    private final String email;
+    private final String name;
+    private final Long studentId;
+    private final Sex sex;
+}
